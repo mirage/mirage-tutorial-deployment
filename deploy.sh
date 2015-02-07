b4b7739bd4e9cb23f59595328d381fc5d2fba043
Richard Mortier <mort@cantab.net>
2015-02-07 16:54:04 +0000

    scripts: add deployment script and config template

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -ex
+
+VM=mir-tutorial.xen
+KERNEL=`pwd`/xen/`cat xen/latest`
+
+sed -e "s,@KERNEL@,$KERNEL/${VM}.xen,g" < xl.conf.in > $KERNEL/xl.conf
+
+cd $KERNEL
+rm -f $VM.xen
+bunzip2 -k ${VM}.xen.bz2
+
+sudo xl destroy $VM || true
+sudo xl create xl.conf
