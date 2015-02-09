2e400725b63a1e29a3e99dbec474da44f682cd79
Richard Mortier <mort@cantab.net>
2015-02-09 12:19:21 +0000

    scripts: parameterise more

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,11 @@
 
 VM=mir-tutorial.xen
 KERNEL=`pwd`/xen/`cat xen/latest`
-
-sed -e "s,@KERNEL@,$KERNEL/${VM}.xen,g" < xl.conf.in > $KERNEL/xl.conf
+sed -e "s,@VM@,$VM,g; s,@KERNEL@,$KERNEL/$VM.xen,g" <xl.conf.in >$KERNEL/xl.conf
 
 cd $KERNEL
 rm -f $VM.xen
-bunzip2 -k ${VM}.xen.bz2
+bunzip2 -k $VM.xen.bz2
 
 sudo xl destroy $VM || true
 sudo xl create xl.conf
